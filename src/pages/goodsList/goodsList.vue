26b3adf62ecea102873c407394f905a692186906
zqh <2371474513@qq.com>
2025-08-10 12:43:41 +0800

    [Feature]新增过滤功能

--- src/pages/goodsList/goodsList.vue
+++ src/pages/goodsList/goodsList.vue
@@ -1,14 +1,19 @@
 <template>
   <view>
-    <view class="search"><up-search :show-action="false" placeholder="日照香炉生紫烟"></up-search></view>
+    <view class="search"><up-search focus @search="clickSearch" @clear="handleClear" @change="handleChange"
+        :show-action="false" placeholder="搜索商品"></up-search>
+    </view>
     <scroll-view scroll-y scroll-top>
       <view class="goodsList">
 
-        <view v-for="(item, i) in goodsList" class="goodsItem">
-          <img :src="item.imgPath" />
-          <text class="name">{{ item.name }}</text>
-          <text class="price">{{ `价格: ${item.price} 元` }}</text>
+        <view v-for="(item, i) in targetList" class="goodsItem">
+          <up-image :show-loading="true" :src="item.imgPath" width="100%" radius="10px" height="150px"
+            lazyLoad></up-image>
 
+          <view class="priceName">
+            <text class="name">{{ item.name }}</text>
+            <text class="price">{{ `价格: ${item.price} 元` }}</text>
+          </view>
         </view>
       </view>
     </scroll-view>
@@ -20,18 +25,41 @@
 import { onMounted, ref } from 'vue';
 import { getGoodsList } from '../../api/index';
 
-const goodsList = ref([]) as any
-
+const goodsList = ref([]) as any;
+const targetList = ref([]) as any;
 
 onMounted(() => {
   (async () => {
     const goodsListRes = await getGoodsList();
     if (goodsListRes) {
       goodsList.value = goodsListRes;
+      targetList.value = goodsListRes;
     }
   })()
 
 })
+
+const initData = () => {
+  targetList.value = goodsList.value;
+}
+
+const clickSearch = (value: string) => {
+  targetList.value = goodsList.value.filter((item: any) => {
+    return String(item.name).indexOf(value) !== -1
+  });
+
+
+}
+
+const handleChange = (value: string) => {
+  if (!value) {
+    initData()
+  }
+}
+
+const handleClear = () => {
+  initData()
+}
 
 
 </script>
@@ -44,7 +72,7 @@ onMounted(() => {
 }
 
 .goodsList {
-  height: 500px;
+  height: 800px;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
   grid-gap: 10px 20px;
@@ -54,25 +82,34 @@ onMounted(() => {
     width: 100%;
     height: 100%;
     border-radius: 10px;
+    box-shadow: 1px 1px 5px 0 #808080;
+    max-height: 220px;
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    justify-content: space-between;
 
-    .name {
+    .priceName {
+      margin: 0 0 15px 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
       align-items: center;
-      margin-bottom: 5px;
 
+      .name {
+        margin-bottom: 5px;
+
+      }
+
+      .price {
+        color: red;
+
+      }
     }
 
-    .price {
-      color: red;
-
-    }
 
     img {
       width: 100%;
-      height: 200px;
+      height: 150px;
       border-radius: 10px;
       margin-bottom: 10px;
     }
